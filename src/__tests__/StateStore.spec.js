2dd0677496d22813ec40f4eb7d823ad7a91daf06
Peter Szujewski <pszujewski@gmail.com>
2019-02-24 16:31:44 -0500

    Adds testing-utils and first test

--- src/__tests__/StateStore.spec.js
+++ src/__tests__/StateStore.spec.js
@@ -1,7 +1,37 @@
+import React from "react";
 import { StateStore } from "../StateStore";
+import ComponentTestSetup from "../testing-utils/ComponentTestSetup";
+
+const getProps = (initialState = { counter: 0 }) => ({
+  reducer: (state = initialState, action) => {
+    switch (action.type) {
+      case "INCREMENT_COUNTER":
+        return { counter: state.counter + 1 };
+      default:
+        return state;
+    }
+  },
+});
+
+const ExampleStateStore1 = props => {
+  return (
+    <StateStore reducer={props.reducer}>
+      {stateStore => {
+        store = stateStore;
+        return <div id="example-child">Hello StateStore</div>
+      }}  
+    </StateStore>
+  );
+};
 
 describe("<StateStore />", () => {
-  it("is true", () => {
-    expect(true).toBe(true);
+  it("Should accept a function for the children prop", () => {
+    let store = {}; 
+
+    const setup = new ComponentTestSetup(ExampleStateStore1);
+    const { getByText } = setup.renderForTest(getProps());
+
+    expect(store.getState()).toEqual({ counter: 0 });
+    expect(getByText("Hello StateStore")).toBeInTheDocument();
   });
 });
---
+++ src/testing-utils/ComponentTestSetup.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { render } from "react-testing-library";
+
+export default class ComponentTestSetup {
+    constructor(Component) {
+        this.Component = Component;
+        this.defaultProps = null;
+    }
+
+    setDefaultProps(defaultProps) {
+        this.defaultProps = defaultProps;
+    }
+
+    renderForTest(props) {
+        const Component = this.Component;
+
+        if (!this.defaultProps && props) {
+          return render(<Component {...props} />);
+        }
+
+        if (!this.defaultProps && !props) {
+            return render(<Component />);
+        }
+
+        return render(<Component {...this.mergeProps(props)} />);
+    };
+
+    mergeProps(propsOverride) {
+        const props = this.defaultProps;
+        return Object.assign({}, props, propsOverride);
+    }
+}
